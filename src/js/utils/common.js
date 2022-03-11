af82c4ff951e1c5b79f43fd663e86b7c743c9ef8
YUNHO KANG <ansghkdbsgh@gmail.com>
2022-03-11 17:31:43 +0900

    build

--- src/js/utils/common.js
+++
@@ -1,4 +0,0 @@
-export const changeDateFormat = (date) => {
-  const d = new Date(date);
-  return `${d.getFullYear()}년 ${d.getMonth() + 1}월 ${d.getDate()}일`;
-};
--- src/js/utils/localStorage.js
+++
@@ -1,7 +0,0 @@
-export const saveLocalStorage = (key, data) => {
-  localStorage.setItem(key, JSON.stringify(data));
-};
-
-export const getLocalStorage = (key) => {
-  return JSON.parse(localStorage.getItem(key)) || [];
-};
